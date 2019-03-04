3284eff26bf2c03c936f20c2fa8cde0cd71b0313
ghoiufyia <ghoiufyia@163.com>
2019-03-04 17:14:54 +0800

    shadowsocks

--- shadowsocks/repair.sh
+++ shadowsocks/repair.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i '/EVP_CIPHER_CTX_cleanup/s@EVP_CIPHER_CTX_cleanup@EVP_CIPHER_CTX_reset@' /usr/lib/python3.7/site-packages/shadowsocks/crypto/openssl.py
+sed -i '/EVP_CIPHER_CTX_cleanup/s@EVP_CIPHER_CTX_cleanup@EVP_CIPHER_CTX_reset@' /usr/local/lib/python3.7/site-packages/shadowsocks/crypto/openssl.py
